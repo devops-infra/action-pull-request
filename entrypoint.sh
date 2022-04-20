9f9e73c823b0ede8fe0a3406deda8eb91646cc6a
Krzysztof Szyper <45788587+ChristophShyper@users.noreply.github.com>
2022-04-20 09:30:19 +0200

    Remove sudo for git safe.directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,8 +43,6 @@ fi
 echo -e "\nSetting GitHub credentials..."
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 git config --global --add safe.directory /github/workspace
-sudo git config --global --add safe.directory "${GITHUB_WORKSPACE}"
-sudo git config --global --add safe.directory /github/workspace
 git remote set-url origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
